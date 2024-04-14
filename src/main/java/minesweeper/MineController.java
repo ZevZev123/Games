c4a257af1c588b5bd5e5e43f8695da6b1fc3174f
ZevZev <davidzahariev@gmail.com>
2024-04-14 13:28:50 +0200

    testo utilizzato per identificare il contenuto del bottone

--- src/main/java/minesweeper/MineController.java
+++ src/main/java/minesweeper/MineController.java
@@ -34,6 +34,8 @@ public class MineController {
         for (int row = 0; row < maxCol; row++){
             for (int col = 0; col < maxRow; col++){
                 button = new Button();
+                button.setOnAction(event -> leftButtonPressed(event));
+                /*
                 button.setOnMousePressed(event -> {
                         if (event.getButton() == MouseButton.PRIMARY) {
                             leftButtonPressed(event);
@@ -43,6 +45,7 @@ public class MineController {
                 button.setOnContextMenuRequested(event -> {
                     putFlag(event);
                 });
+                */
                 button.getStyleClass().setAll("button-game");
 
                 matrix[row][col] = button;
@@ -60,15 +63,20 @@ public class MineController {
             generate(position);
             isGenerated = true;
         }
-        if(matrix[position[0]][position[1]].getStyleClass().contains("bomb")){
+        if(matrix[position[0]][position[1]].getText() == "bomb"){
+            disableButtons();
             System.out.println("Game Over");
         }
         
     }
     
-    private void putFlag(ActionEvent event){
+    private void flag(ActionEvent event){
         int[] position = getPosition((Button) event.getSource());
-        matrix[position[0]][position[1]].getStyleClass().addAll("mine");
+        if (matrix[position[0]][position[1]].getStyleClass().contains("mine")){
+            matrix[position[0]][position[1]].getStyleClass().addAll("mine");
+        } else {
+            matrix[position[0]][position[1]].getStyleClass().setAll("button-game");
+        }
     }
 
     private int[] getPosition(Button button){
@@ -92,19 +100,20 @@ public class MineController {
             y = rand.nextInt(maxRow);
 
             if ((x < (position[0]-1) || x > (position[0]+1)) || (y < (position[1]-1) || y > (position[1]+1))){
-                matrix[x][y].getStyleClass().addAll("bomb");
+                matrix[x][y].setText("bomb");
+                //matrix[x][y].getStyleClass().addAll("bomb");
                 count++;
             }
         }
 
         for (int col = 0; col < maxCol; col++){
             for (int row = 0; row < maxRow; row++){
-                if (!matrix[col][row].getStyleClass().contains("bomb")){
+                if (matrix[col][row].getText() != "bomb"){
                     count = 0;
                     for (int col2 = col-1; col2 < col+2; col2++){
                         for (int row2 = row-1; row2 < row+2; row2++){
                             if (col2 >= 0 && row2 >= 0 && col2 < maxCol && row2 < maxRow){
-                                if (matrix[col2][row2].getStyleClass().contains("bomb")){
+                                if (matrix[col2][row2].getText() == "bomb"){
                                     count++;
                                 }        
                             }
@@ -112,28 +121,36 @@ public class MineController {
                     }
                     switch (count) {
                         case 1:
-                            matrix[col][row].getStyleClass().addAll("bomb1");
+                            matrix[col][row].setText("bomb1");
+                            //matrix[col][row].getStyleClass().addAll("bomb1");
                             break;
                         case 2:
-                            matrix[col][row].getStyleClass().addAll("bomb2");
+                            matrix[col][row].setText("bomb2");
+                            //matrix[col][row].getStyleClass().addAll("bomb2");
                             break;
                         case 3:
-                            matrix[col][row].getStyleClass().addAll("bomb3");
+                            matrix[col][row].setText("bomb3");
+                            //matrix[col][row].getStyleClass().addAll("bomb3");
                             break;
                         case 4:
-                            matrix[col][row].getStyleClass().addAll("bomb4");
+                            matrix[col][row].setText("bomb4");
+                            //matrix[col][row].getStyleClass().addAll("bomb4");
                             break;
                         case 5:
-                            matrix[col][row].getStyleClass().addAll("bomb5");
+                            matrix[col][row].setText("bomb5");
+                            //matrix[col][row].getStyleClass().addAll("bomb5");
                             break;
                         case 6:
-                            matrix[col][row].getStyleClass().addAll("bomb6");
+                            matrix[col][row].setText("bomb6");
+                            //matrix[col][row].getStyleClass().addAll("bomb6");
                             break;
                         case 7:
-                            matrix[col][row].getStyleClass().addAll("bomb7");
+                            matrix[col][row].setText("bomb7");
+                            //matrix[col][row].getStyleClass().addAll("bomb7");
                             break;
                         case 8:
-                            matrix[col][row].getStyleClass().addAll("bomb8");
+                            matrix[col][row].setText("bomb8");
+                            //matrix[col][row].getStyleClass().addAll("bomb8");
                             break;
                     
                         default:
@@ -157,6 +174,7 @@ public class MineController {
         for (Button[] list: matrix){
             for (Button button: list){
                 button.getStyleClass().setAll("button-game");
+                button.setText("");
                 button.setDisable(false);
                 isGenerated = false;
             }
