bbec9faa7210103520e0dff421282e23aab74f83
ZevZev <davidzahariev@gmail.com>
2024-04-18 11:53:04 +0200

    fatta la funzione move, da finire checkwin

--- src/main/java/tris/TrisController.java
+++ src/main/java/tris/TrisController.java
@@ -3,13 +3,15 @@ package tris;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class TrisController {
-    @FXML
-    private GridPane matrice;
+    @FXML private GridPane matrice;
+    @FXML private Label label;
 
     private Button[][] matrix = new Button[3][3];
+    private boolean xTurn = true;
 
     @FXML
     private void initialize(){
@@ -27,12 +29,28 @@ public class TrisController {
     }
 
     private void move(ActionEvent event){
-        
-
+        Button button = (Button) event.getSource();
+        if (xTurn){
+            if (button.getStyleClass().size() != 2){
+                button.getStyleClass().addAll("x-button");
+                xTurn = false;
+                label.setText("Turn: O");
+            }
+        } else {
+            if (button.getStyleClass().size() != 2){
+                button.getStyleClass().addAll("o-button");
+                xTurn = true;
+                label.setText("Turn: X");
+            }
+        }
         checkWin();
     }
 
     private void checkWin(){
-
+        for (int rows = 0; rows < 3; rows++){
+            if (matrix[0][rows].getStyleClass().contains("x-button") && matrix[1][rows].getStyleClass().contains("x-button") && matrix[2][rows].getStyleClass().contains("x-button")){
+                System.out.println("X wins");
+            }
+        }
     }
 }
