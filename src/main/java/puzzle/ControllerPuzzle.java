40278b0f96696aebeb7aeb1437236f04f9721e80
ZevZev <davidzahariev@gmail.com>
2024-04-19 16:41:24 +0200

    test taglio immagine

--- src/main/java/puzzle/ControllerPuzzle.java
+++ src/main/java/puzzle/ControllerPuzzle.java
@@ -1,5 +1,43 @@
 package puzzle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+
 public class ControllerPuzzle {
-    
+    @FXML private GridPane matrice;
+
+    private Button[][] matrix = new Button[8][4];
+
+    @FXML
+    private void initialize(){
+        Image originalImage = new Image("../images/orangecar.jpeg");
+
+        // Dimensioni dell'immagine originale
+        double originalWidth = originalImage.getWidth();
+        double originalHeight = originalImage.getHeight();
+
+        // Definisci le dimensioni dei pezzi dell'immagine
+        double pieceWidth = originalWidth / 8;
+        double pieceHeight = originalHeight / 4;
+
+        System.out.println("width: " + pieceWidth);
+        System.out.println("height: " + pieceHeight);
+
+        /*
+        ImageView subImages = new ImageView(originalImage);
+        for (int row = 0; row < pieceHeight*3; row = row + (int) pieceHeight){
+            for (int col = 0; col < pieceWidth*7; col = col + (int) pieceWidth){
+                subImages.setViewport(new javafx.geometry.Rectangle2D(col, row, pieceWidth, pieceHeight));
+            }
+        }
+        */
+        
+    }
+
+    @FXML
+    private void shuffle(){
+        System.out.println("SHUFFLE");
+    }
 }
--- src/main/java/puzzle/MainPuzzle.java
+++ src/main/java/puzzle/MainPuzzle.java
@@ -7,13 +7,13 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class MinePuzzle extends Application {
+public class MainPuzzle extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/puzzle/structure.fxml"));
         primaryStage.setTitle("Minesweeper");
-        primaryStage.getIcons().add(new Image("file:src\\main\\resources\\images\\minesweeper.png"));
+        primaryStage.getIcons().add(new Image("file:src\\main\\resources\\images\\puzzle.png"));
         Scene scene = new Scene(root, 700, 600);
         scene.getStylesheets().add(getClass().getResource("/puzzle/style.css").toExternalForm());
         primaryStage.setScene(scene);
